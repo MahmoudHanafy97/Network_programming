3d5dab68eb9579cc19d4198dea0373876291bda6
MahmoudHanafy97 <134972406+MahmoudHanafy97@users.noreply.github.com>
2023-05-31 15:57:38 +0300

    Add files via upload

---
+++ Codes/Time_Server/timeServer.py
@@ -0,0 +1,11 @@
+import socket
+S = socket.socket (socket.AF_INET, socket. SOCK_STREAM)
+S.bind((socket.gethostname(), 1025))
+S.listen (5)
+import datetime
+dateAsString = str(datetime.datetime.now())
+x=dateAsString.encode()
+while True:
+    clt, adr = S.accept()
+    print (f"Connection to {adr} is established")
+    clt.send(x)
